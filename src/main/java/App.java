94ff35bdeccba85658d84724bd35342a4dff8645
igorciz777 <igorciz777@gmail.com>
2021-04-27 14:42:25 +0200

    Setting up starting a round

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,21 @@
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App {
+    private Scanner scanner;
+    private Game game;
+
+    App(){
+        scanner = new Scanner(System.in);
+        game = new Game();
+    }
+
+    public void menu() throws IOException, ParseException {
+        System.out.println("How many decks of cards?:");
+        game.startNewGame(scanner.nextInt());
+        game.startRound();
+        game.printCards();
+    }
 }
